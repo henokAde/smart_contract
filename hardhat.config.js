42c83e2bb5ebdde97f22966d1f1a196670503756
henokADE <adeyamah@yahoo.fr>
2022-01-06 10:43:40 +0100

    contract ready to deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,12 @@
-require("@nomiclabs/hardhat-waffle");
+// https://eth-ropsten.alchemyapi.io/v2/qEo6H-ePIngHZWUy0xtxOEyDleTzq2Ox
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+require('@nomiclabs/hardhat-waffle');
 module.exports = {
-  solidity: "0.8.4",
-};
+  solidity: '0.8.0',
+  neword : {
+    ropsten :{
+      url: 'https://eth-ropsten.alchemyapi.io/v2/qEo6H-ePIngHZWUy0xtxOEyDleTzq2Ox',
+      account: ['4b2cd639dc20aa4c6b1c90e9eb6943af45c8e5215832b6607c65f727f20fa807']
+    }
+  }
+}

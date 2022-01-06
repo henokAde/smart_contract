b249b215b1c0bc3f6ce212e2f2869e6dd2f375bf
henokADE <adeyamah@yahoo.fr>
2022-01-06 10:55:57 +0100

    fixing syntax fault

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,23 @@
-// https://eth-ropsten.alchemyapi.io/v2/qEo6H-ePIngHZWUy0xtxOEyDleTzq2Ox
-
 require('@nomiclabs/hardhat-waffle');
 module.exports = {
   solidity: '0.8.0',
-  neword : {
+  networks : {
     ropsten :{
       url: 'https://eth-ropsten.alchemyapi.io/v2/qEo6H-ePIngHZWUy0xtxOEyDleTzq2Ox',
-      account: ['4b2cd639dc20aa4c6b1c90e9eb6943af45c8e5215832b6607c65f727f20fa807']
+      accounts: ['4b2cd639dc20aa4c6b1c90e9eb6943af45c8e5215832b6607c65f727f20fa807'],
     }
   }
-}
+};
+
+// require('@nomiclabs/hardhat-waffle');
+
+// module.exports = {
+//   solidity: '0.8.0',
+//   networks: {
+//     ropsten: {
+//       url: 'https://eth-ropsten.alchemyapi.io/v2/qEo6H-ePIngHZWUy0xtxOEyDleTzq2Ox',
+//       accounts: ['4b2cd639dc20aa4c6b1c90e9eb6943af45c8e5215832b6607c65f727f20fa807'],
+//     },
+//   },
+// };
+
